d3b9c8bddfa03867f83b98b079987b58ca172522
josephbae23 <yousufjamil2003@gmail.com>
2026-03-26 00:31:31 +0600

    Simplify next.config.js - remove basePath/static export for Vercel

--- next.config.js
+++ next.config.js
@@ -1,25 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS === "true";
-const repositoryName = process.env.GITHUB_REPOSITORY
-  ? process.env.GITHUB_REPOSITORY.split("/")[1]
-  : "";
-const isUserOrOrgPages = repositoryName.endsWith(".github.io");
-const basePath = isGithubActions && !isUserOrOrgPages ? `/${repositoryName}` : "";
-
 const nextConfig = {
-  // Only use static export for GitHub Pages, not for Vercel
-  ...(isGithubActions && {
-    output: "export",
-    trailingSlash: true,
-    images: {
-      unoptimized: true,
-    },
-  }),
-  basePath,
-  assetPrefix: basePath || undefined,
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
-  },
   serverExternalPackages: [],
 }
 
