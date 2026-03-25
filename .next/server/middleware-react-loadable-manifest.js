57694d74fcb2752ffd236ddb4a3fecba29566f5f
josephbae23 <yousufjamil2003@gmail.com>
2026-03-25 23:36:55 +0600

    Remove .next directory from git

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"components\\\\sections\\\\OfficesMap.tsx -> leaflet\":{\"id\":\"components\\\\sections\\\\OfficesMap.tsx -> leaflet\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_leaflet_dist_leaflet-src_js.js\"]}}"
